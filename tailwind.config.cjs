bf1d0b5187b6725437e8d33d4857781a109dd144
M Inam <34743206+minamme@users.noreply.github.com>
2023-10-03 15:13:24 +0500

    new tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors')
+
+const shade = 'slate'
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,json}'],
 	theme: {
 		extend: {
+			colors: {
+				text: {
+					dim: colors[shade]['500'],
+					DEFAULT: colors[shade]['700'],
+					active: colors[shade]['900'],
+				},
+				bg: colors[shade]['100'],
+				dark: {
+					text: {
+						dim: colors[shade]['600'],
+						DEFAULT: colors[shade]['300'],
+						active: colors[shade]['100'],
+					},
+					bg: colors[shade]['900'],
+				}
+			},
 			fontFamily: {
 				sans: ['Inter Variable', ...defaultTheme.fontFamily.sans], // Set default font to Inter
 			},
