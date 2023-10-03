d3ea76b547d3efddb36f0b8bba513bb81df89bd9
M Inam <34743206+minamme@users.noreply.github.com>
2023-10-03 10:19:28 +0500

    header done

--- src/ts/menu.ts
+++ src/ts/menu.ts
@@ -1,23 +1,15 @@
-function activeLinkWithScroll(
-  navLinks: NodeListOf<HTMLAnchorElement>,
-  sections: NodeListOf<HTMLElement>
-) {
-  const observer = new IntersectionObserver(setActiveLink, {
-    rootMargin: "-40% 0px -50% 0px",
-    root: null,
-  });
-  sections.forEach(section => observer.observe(section));
-  function setActiveLink(entries: IntersectionObserverEntry[]) {
-    const interSecting = entries.filter(e => e.isIntersecting).map(e => e.target.id)[0];
-    navLinks.forEach(link => {
-      const curr = link.href.includes(interSecting) ? "add" : "remove";
-
-      link?.querySelector("span")?.classList[curr]("w-16");
-    });
-  }
-}
-
 const navLinks = document.querySelectorAll("nav a") as NodeListOf<HTMLAnchorElement>;
-const sections = document.querySelectorAll("main > section") as NodeListOf<HTMLElement>;
 
-activeLinkWithScroll(navLinks, sections);
+const options = { rootMargin: "-40% 0px -50% 0px", root: null };
+const observer = new IntersectionObserver(setActiveLink, options);
+
+document.querySelectorAll("main > section").forEach(section => observer.observe(section));
+
+function setActiveLink(entries: IntersectionObserverEntry[]) {
+  const interSecting = entries.filter(e => e.isIntersecting).map(e => e.target.id)[0];
+
+  navLinks.forEach(link => {
+    const span = link.querySelector("span");
+    if (span) span.style.width = link.href.includes(interSecting) ? "4rem" : "";
+  });
+}
