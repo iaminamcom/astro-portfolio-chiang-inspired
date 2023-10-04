67b6aeb601598737fa4c72402ca4611094244058
M Inam <34743206+minamme@users.noreply.github.com>
2023-10-04 14:11:24 +0500

    added config to data

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,12 +13,16 @@ const experienceCollection = defineCollection({
 
 const projectCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    image: z.string(),
-    skills: z.array(z.string()),
-    features: z.array(z.object({ name: z.string(), url: z.string(), icon: z.string() })),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      cover: image().refine(img => img.width >= 720, {
+        message: "Cover image must be at least 720 pixels wide!",
+      }),
+      coverAlt: z.string(),
+      skills: z.array(z.string()),
+      features: z.array(z.object({ name: z.string(), url: z.string(), icon: z.string() })),
+    }),
 });
 
 export const collections = {
