1a4280dc245cb96738ef46629edb75338ec907cd
M. Inam <34743206+minamme@users.noreply.github.com>
2023-10-04 22:16:59 +0500

    configed for github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/minamme',
+  base: '/astro-portfolio-chiang-inspired',
   integrations: [tailwind()]
 });
